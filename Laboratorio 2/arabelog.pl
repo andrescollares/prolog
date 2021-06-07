7d47d8f47dcb54c7b1c86f5638838eac3629ec3a
Andrés Collares <andres.collares@fing.edu.uy>
2021-06-07 19:23:07 -0300

    mascomentarios

--- Laboratorio 2/arabelog.pl
+++ Laboratorio 2/arabelog.pl
@@ -152,7 +152,8 @@ mejor_movimiento(Estado, Jugador, Nivel, ia_grupo,  MejorJugada) :-
     mejor_movimiento_step(Estado, Jugador, Jugador, Nivel, -10000, 10000,  MejorJugada, _).
 
 
-%mejor_movimiento_step(+Estado, +JugadorOriginal, +Jugador, +Nivel, +Alpha, +Beta, ?MejorJugada, -MejorPuntaje): 
+%Obtiene todos los movimientos posibles y busca la mejor jugada con profundidad nivel - 1.
+%mejor_movimiento_step(+Estado, +JugadorOriginal, +Jugador, +Nivel, +Alpha, +Beta, -MejorJugada, -MejorPuntaje). 
 mejor_movimiento_step(Estado, JugadorOriginal, Jugador, Nivel, Alpha, Beta, MejorJugada, MejorPuntaje) :-
     Nivel > 0, !,
     Nivel1 is Nivel - 1,
@@ -161,6 +162,7 @@ mejor_movimiento_step(Estado, JugadorOriginal, Jugador, Nivel, Alpha, Beta, Mejo
     movimientos_posibles(Jugador, Estado, MovimientosPosibles),
     mejor_jugada(Nivel1, JugadorOriginal, Jugador, Alpha1, Beta1, MovimientosPosibles, nil, MejorJugada, MejorPuntaje).
     
+%if depth = 0 return the heuristic value of node
 mejor_movimiento_step(Estado, JugadorOriginal, _, _, _, _, _, MejorPuntaje) :-
     tablero_final(0, JugadorOriginal, Estado, MejorPuntaje).
 
@@ -183,6 +185,7 @@ mejor_jugada(Nivel, JugadorOriginal, Jugador, Alpha, Beta, [Jugada| RestoJugadas
     MejorPuntajeHoja1 is -MejorPuntajeHoja,
     cortar(Nivel, JugadorOriginal, Jugador, Alpha, Beta, Jugada, RestoJugadas, JugadaAux, MejorJugada, MejorPuntaje, MejorPuntajeHoja1).
 
+%if node is a terminal node return the heuristic value of node.
 mejor_jugada(_, _, _,Alpha, _, [], Jugada, Jugada, Alpha).
 
 
@@ -192,14 +195,14 @@ mejor_jugada(_, _, _,Alpha, _, [], Jugada, Jugada, Alpha).
 cortar(_, _, _, _, Beta, Jugada, _, _, Jugada, MejorPuntajeHoja, MejorPuntajeHoja) :-
     MejorPuntajeHoja >= Beta, !.
 
-%Alpha = max(Alpha, minimax(child,...)), minimax > Alpha.
-%Beta = min(Beta, minimax(child,...)), minimax < Beta.
+%Alpha = max(Alpha, minimax(child,...)), minimax > Alpha. return minimax
+%Beta = min(Beta, minimax(child,...)), minimax < Beta. return minimax 
 cortar(Nivel, JugadorOriginal, Jugador, Alpha, Beta, Jugada, RestoJugadas, _, MejorJugadaActual, MejorPuntajeActual, MejorPuntajeHoja) :-
     Alpha < MejorPuntajeHoja, MejorPuntajeHoja < Beta, !,
     mejor_jugada(Nivel, JugadorOriginal , Jugador, MejorPuntajeHoja, Beta, RestoJugadas, Jugada, MejorJugadaActual, MejorPuntajeActual).
 
-%Alpha = max(Alpha, minimax(child,...)), minimax < Alpha.
-%Beta = min(Beta, minimax(child,...)), minimax > Beta.
+%Alpha = max(Alpha, minimax(child,...)), minimax < Alpha. return Alpha
+%Beta = min(Beta, minimax(child,...)), minimax > Beta. return Beta
 cortar(Nivel, JugadorOriginal, Jugador, Alpha, Beta, _, RestoJugadas, JugadaAux, MejorJugadaActual, MejorPuntajeActual, MejorPuntajeHoja) :-
     MejorPuntajeHoja =< Alpha, !,
     mejor_jugada(Nivel, JugadorOriginal, Jugador, Alpha, Beta, RestoJugadas, JugadaAux, MejorJugadaActual, MejorPuntajeActual).
